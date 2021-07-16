65ae8a8dab1027c2e7b53f1d8e6b9ea39aa8dd15
JiyeonMin <ichbinmin2@gmail.com>
2021-07-16 18:28:00 +0900

    Add : Upload page 입력폼 추가

--- client/src/components/views/UploadProductPage/UploadProdectPage.js
+++ client/src/components/views/UploadProductPage/UploadProdectPage.js
@@ -1,40 +1,75 @@
-import React from 'react'
-import {Typography, Button, Input, Form} from 'antd';
+import React, { useState } from "react";
+import { Typography, Button, Input, Form } from "antd";
 
-const {Title} = Typography;
-const {TextArea} = Input;
+const { Title } = Typography;
+const { TextArea } = Input;
+
+const continents = [
+  { key: 1, value: "Africa" },
+  { key: 2, value: "Europe" },
+  { key: 3, value: "Asia" },
+  { key: 4, value: "North America" },
+  { key: 5, value: "South America" },
+  { key: 6, value: "Australia" },
+  { key: 7, value: "Antarctica" },
+];
+
 function UploadProdectPage() {
-    return (
-        <div style={{ maxWidth: '700px', margin: "2rem auto"}}>
-            <div style={{ textAlign: 'center', marginBottom: '2rem'}}>
-                <Title level={2}> 여행 상품 업로드</Title>
-            </div>
-            <Form>
-                <br />
-                <br />
-                <label>이름</label>
-                <Input />
-                <br />
-                <br />
-                <label>설명</label>
-                <TextArea />
-                <br />
-                <br />
-                <label>가격($)</label>
-                <Input />
-                <br />
-                <br />
-                <select>
-                    <option></option>
-                </select>
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState(0);
+  const [continent, setContinent] = useState(1);
+  const [images, setimages] = useState([]);
 
-                <br />
-                <br />
-                <Button>확인</Button>
-            </Form>
-           
-        </div>
-    )
+  const titleChangeHandler = (event) => {
+    setTitle(event.currentTarget.value);
+  };
+
+  const descriptionChangeHandler = (event) => {
+    setDescription(event.currentTarget.value);
+  };
+
+  const priceChangeHandler = (event) => {
+    setPrice(event.currentTarget.value);
+  };
+
+  const continentChangeHandler = (event) => {
+    setContinent(event.currentTarget.value);
+  };
+
+  return (
+    <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
+      <div style={{ textAlign: "center", marginBottom: "2rem" }}>
+        <Title level={2}> 여행 상품 업로드</Title>
+      </div>
+      <Form>
+        <br />
+        <br />
+        <label>이름</label>
+        <Input onChange={titleChangeHandler} value={title} />
+        <br />
+        <br />
+        <label>설명</label>
+        <TextArea onChange={descriptionChangeHandler} value={description} />
+        <br />
+        <br />
+        <label>가격($)</label>
+        <Input type="number" onChange={priceChangeHandler} value={price} />
+        <br />
+        <br />
+        <select onChange={continentChangeHandler} value={continent}>
+          {continents.map((item) => (
+            <option key={item.key} value={item.key}>
+              {item.value}
+            </option>
+          ))}
+        </select>
+        <br />
+        <br />
+        <Button>확인</Button>
+      </Form>
+    </div>
+  );
 }
 
-export default UploadProdectPage
+export default UploadProdectPage;
