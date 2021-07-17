d27cd565140a9b6da7f5e522575d308e50b0bc87
JiyeonMin <ichbinmin2@gmail.com>
2021-07-17 15:24:56 +0900

    Add : multer 추가 생성

--- client/src/components/utils/FileUpload.js
+++ client/src/components/utils/FileUpload.js
@@ -1,8 +1,25 @@
 import React from "react";
 import Dropzone from "react-dropzone";
 import { Icon } from "antd";
+import axios from "axios";
 
 function FileUpload() {
+  const dropHandler = (files) => {
+    let formData = new FormData();
+    const config = {
+      header: { "content-type": "multipart/form-data" },
+    };
+    formData.append("file", files[0]);
+
+    // 엔드포인트 "/api/product/image"
+    axios.post("/api/product/image", formData, config).then((response) => {
+      if (response.data.success) {
+      } else {
+        alert("파일을 저장하는데 실패했습니다.");
+      }
+    });
+  };
+
   return (
     <div style={{ display: "flex", justifyContent: "space-between" }}>
       <Dropzone onDrop={(acceptedFiles) => console.log(acceptedFiles)}>
---
+++ server/routes/product.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+//=================================
+//             Product
+//=================================
+
+router.post("/image", (req, res) => {
+  // 가져온 이미지를 저장해는 작업
+});
+
+module.exports = router;
