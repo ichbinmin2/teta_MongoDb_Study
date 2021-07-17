f2bc633b35e659c8729311f37ace59a0f3f852f1
JiyeonMin <ichbinmin2@gmail.com>
2021-07-17 18:28:08 +0900

    Add : MongoDB Product data 저장 기능 추가

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { Typography, Button, Input, Form } from "antd";
+import { Typography, Button, Input, Form, Descriptions } from "antd";
 import FileUpload from "../../utils/FileUpload";
+import Axios from "axios";
 
 const { Title } = Typography;
 const { TextArea } = Input;
@@ -15,7 +16,7 @@ const continents = [
   { key: 7, value: "Antarctica" },
 ];
 
-function UploadProductPage() {
+function UploadProductPage(props) {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [price, setPrice] = useState(0);
@@ -42,13 +43,40 @@ function UploadProductPage() {
     setimages(newImages);
   };
 
+  const submitHandler = (event) => {
+    // event.preventDafault();
+    // 유효성 검사
+    if (!title || !description || !price || !continent || !images) {
+      return alert("모든 정보를 작성해주셔야 합니다.");
+    }
+
+    // 서버에 채운 값들을 request로 보낸다
+    const body = {
+      writer: props.user.userData._id,
+      title: title,
+      description: description,
+      price: price,
+      images: images,
+      continent: continent,
+    };
+
+    Axios.post("/api/product", body).then((response) => {
+      if (response.data.success) {
+        alert("상품 업로드에 성공했습니다.");
+        props.history.push("/");
+      } else {
+        alert("상품 업로드에 실패했습니다.");
+      }
+    });
+  };
+
   return (
     <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
       <div style={{ textAlign: "center", marginBottom: "2rem" }}>
         <Title level={2}> 여행 상품 업로드</Title>
       </div>
 
-      <Form>
+      <Form onSubmit={submitHandler}>
         <FileUpload refreshFunction={updateImages} />
         <br />
         <br />
@@ -73,7 +101,9 @@ function UploadProductPage() {
         </select>
         <br />
         <br />
-        <Button>확인</Button>
+        <Button type="submit" onClick={submitHandler}>
+          확인
+        </Button>
       </Form>
     </div>
   );
--- server/routes/product.js
+++ server/routes/product.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
-
+const { Product } = require("../models/Product");
 //=================================
 //             Product
 //=================================
@@ -31,4 +31,15 @@ router.post("/image", (req, res) => {
   });
 });
 
+// index.js에서 맞춰준 포인트가 있기 때문에 굳이 /api/product 를 전부다 작성할 필요가 없음.
+router.post("/", (req, res) => {
+  // 받아온 정보들을 db에 넣어주는 작업
+
+  const product = new Product(req.body);
+
+  product.save((err) => {
+    if (err) return res.status(400).json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
+});
 module.exports = router;
